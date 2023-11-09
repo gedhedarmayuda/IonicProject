4aa4083a1e1e738196b77ffbf5b74dca5cc6b572
Gede Darma Yuda <gedheyuda398@gmail.com>
2023-11-09 19:21:21 +0800

    finish

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  t2: number = 0;
+  a: number = 0;
+  g: number = 0;
+  m1: number = 0;
+  m2: number = 0;
 
   constructor() {}
-
+  keplerThird(){
+    this.t2 = (4*Math.pow(Math.PI, 2)/this.g*(this.m1+this.m2))*Math.pow(this.a, 3)
+  }
+  reset(){
+    this.a = 0;
+    this.g = 0;
+    this.m1 = 0;
+    this.m2 = 0;
+    this.t2 = 0;
+  }
 }
