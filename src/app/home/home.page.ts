51453b1d3009c1927d35d814ace72039ea8eb273
Gede Darma Yuda <gedheyuda398@gmail.com>
2023-12-01 13:50:26 +0800

    commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,27 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  Question1: string = "";
-  remarks1: string = "";
 
   constructor() {}
 
-  SeeAnswer1(){
-    switch(this.Question1){
-      case 'a':
-        this.remarks1 = "Elephants are huge but not tallest";
-        break;
-      case 'b':
-        this.remarks1 = "Hippo is huge but cannot reach tree";
-        break;
-      case 'c':
-        this.remarks1 = "You got it right!";
-        break;
-      case 'd':
-        this.remarks1 = "Zebra only reach grass and small tree";
-        break;
-      default:
-        this.remarks1 = "";
-    }
-  }
 }
